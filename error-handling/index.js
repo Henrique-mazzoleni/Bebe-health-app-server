d2575ce92354da82d7c58d04bf848c223ca2614b
Henrique-mazzoleni <henrique.mazzoleni@gmail.com>
2023-04-01 14:28:38 +0200

    fixed unnecessary api calls

--- error-handling/index.js
+++ error-handling/index.js
@@ -11,9 +11,14 @@ module.exports = (app) => {
 
     // only render if the error ocurred before sending the response
     if (!res.headersSent) {
-      res.status(500).json({
-        message: "Internal server error. Check the server console",
-      });
+      if (err.code === "invalid_token")
+        res
+          .status(401)
+          .json({ message: "invalid token, try logging in again." });
+      else
+        res.status(500).json({
+          message: "Internal server error. Check the server console",
+        });
     }
   });
 };
